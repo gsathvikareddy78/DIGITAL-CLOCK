dbd48fd5fc0a871492a26f4c1376270c47fb5b05
sathvika <gsathvikareddy78@gmail.com>
2024-03-29 22:24:39 +0530

    Fisr commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let hour=document.getElementById("hours");
+let minute=document.getElementById("minutes");
+let second=document.getElementById("seconds");
+let ampm1=document.getElementById("ampm");
+
+function updateClock(){
+    let hours=new Date().getHours();
+    let minutes=new Date().getMinutes();
+    let seconds=new Date().getSeconds();
+
+    let ampm="AM";
+    if(hours>=12){
+        ampm="PM";
+    }
+    second=(second<10)?"0"+second:second; 
+
+    hour.innerText=hours;
+    minute.innerText=minutes;
+    second.innerText=seconds;
+    ampm1.innerText=ampm;
+
+    setTimeout(() => {
+        updateClock()
+    }, 1000);
+
+}
+updateClock();
